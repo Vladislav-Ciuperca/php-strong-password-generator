532741d05cb26dce816b4e1d93a6da6e80ec585c
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-06-13 23:01:26 +0200

    commit1

--- elabora.php
+++ elabora.php
@@ -2,19 +2,33 @@
 
 $length = $_GET["number"];
 
+$valore = "cioa ";
+$balore = "bello ";
+
+function prova($arg1, $arg2)
+{
+    echo $arg1;
+    echo $arg2;
+}
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<h1><?php echo $length ?></h1>
+
+    <h1><?php echo $length ?></h1>
+
+    <h2><?php prova($valore, $balore) ?></h2>
 
 </body>
+
 </html>
